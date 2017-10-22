46438bbf049ad2bbaf24b7f233bbbf0e742eb709
FACON Yohann <facon.y@paris.sfeir.com>
2017-10-22 19:57:51 +0200

    create log directory

--- helpers/logger.js
+++ helpers/logger.js
@@ -1,8 +1,16 @@
 const winston = require('winston')
 const path = require('path')
 const config = require('config')
+const fs = require('fs')
+const { promisify } = require('util')
+
+const accessAsync = promisify(fs.access)
 
 const logFilePath = [__dirname, '..', ...config.get('logFilePath')]
+
+const dirLog = path.join(...logFilePath.slice(0, -1))
+
+ensureFile(dirLog)
 
 const logger = new (winston.Logger)({
   transports: [
@@ -10,5 +18,14 @@ const logger = new (winston.Logger)({
     new (winston.transports.File)({ filename: path.join(...logFilePath) })
   ]
 })
+
+async function ensureFile (checkFile) {
+  try {
+    await accessAsync(dirLog)
+  } catch (err) {
+    const mkDirAsync = promisify(fs.mkdir)
+    await mkDirAsync(dirLog)
+  }
+}
 
 module.exports.logger = logger
